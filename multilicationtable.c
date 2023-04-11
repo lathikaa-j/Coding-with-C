54972503956ac9d9113f048f31a5ec09c34790c2
lathikaa-j <130064380+lathikaa-j@users.noreply.github.com>
2023-04-11 20:11:01 +0530

    Using print statement

---
+++ multilicationtable.c
@@ -0,0 +1,9 @@
+//Multiplication table using print statement
+#include<stdio.h>
+int main()
+{
+    int a=5;
+    printf("Multiplication table of %d is \n %d \n %d \n %d \n %d \n %d",a,a*1,a*2,a*3,a*4,a*5);
+    return 0;
+
+}
