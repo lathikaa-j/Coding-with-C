a53e5d1607902e576ca51ca86f6e852140056305
lathikaa-j <130064380+lathikaa-j@users.noreply.github.com>
2023-04-12 10:01:34 +0530

    Print statement using arithmetic operators

--- arithmeticoperators.c
+++ arithmeticoperators.c
@@ -2,8 +2,15 @@
 #include <stdio.h>
 int main()
 {
-    int a=10, b=5;
-    printf("The sum of the numbers is %d",a+b);
+    int a, b;
+
+    printf("Enter first number:");
+    scanf("%d",&a);
+
+    printf("\n Enter second number:");
+    scanf("%d",&b);
+
+    printf("\n The sum of the numbers is %d",a+b);
     printf("\n");
 
     printf("\n The difference between two numbers is %d",a-b);
