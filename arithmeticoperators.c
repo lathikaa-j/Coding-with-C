963d86e3372dc67f875c48e17cb355114d5c90b7
lathikaa-j <130064380+lathikaa-j@users.noreply.github.com>
2023-04-12 09:54:00 +0530

    Print statement using arithmetic operators

---
+++ arithmeticoperators.c
@@ -0,0 +1,21 @@
+//Arithmetic operators
+#include <stdio.h>
+int main()
+{
+    int a=10, b=5;
+    printf("The sum of the numbers is %d",a+b);
+    printf("\n");
+
+    printf("\n The difference between two numbers is %d",a-b);
+    printf("\n");
+
+    printf("\n The product of two numbers is %d",a*b);
+    printf("\n");
+
+    printf("\n Division of two numbers is %d",a/b);
+    printf("\n");
+
+    printf("\n The remainder of the two numbers is %d",a%b);
+    printf("\n");
+    return 0;
+}
