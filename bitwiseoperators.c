3ebe63b30ac220972d52323f59a309526a63548a
lathikaa-j <130064380+lathikaa-j@users.noreply.github.com>
2023-04-21 16:46:29 +0530

    Print statement using bitwise operators

---
+++ bitwiseoperators.c
@@ -0,0 +1,16 @@
+//Bitwise operators
+
+#include<stdio.h>
+int main()
+{
+    int a=18,b=67; //variable declarations
+
+    printf("a&b = %d\n",a&b);
+    printf("a|b = %d\n",a|b);
+    printf("a^b = %d\n",a^b);
+    printf("~a = %d\n",~a);
+    printf("a>>2 = %d\n",a>>2);
+    printf("b<<1 = %d\n",b<<1);
+
+    return 0;
+}
