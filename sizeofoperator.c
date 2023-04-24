986e51ee4b8ec5eb5395a6630d2a976915c437d7
lathikaa-j <130064380+lathikaa-j@users.noreply.github.com>
2023-04-24 16:39:22 +0530

    Print statement using sizeof operator

---
+++ sizeofoperator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    printf("The size of int a is %d\n",sizeof(a));
+    printf("The size of char a is %d\n",sizeof(char));
+    printf("The size of float a is %d\n",sizeof(float));
+    printf("The size of double a is %d\n",sizeof(double));
+    printf("\n");
+    return 0;
+}
