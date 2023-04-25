c57c19dad65cedc163502d02ba3c8e33952141d1
lathikaa-j <130064380+lathikaa-j@users.noreply.github.com>
2023-04-25 12:46:32 +0530

    Print statement using unary operator

---
+++ unaryoperator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    a=1;
+    a++;
+    printf("The value of a is %d\n",a);
+
+    a= -2;
+    a= -(a);
+    printf("The value of a is %d\n",a);
+
+    a=7;
+    a--;
+    printf("The value of a is %d\n",a);
+
+    a=10;
+    ++a;
+    printf("The value of a is %d\n",a);
+
+
+}
