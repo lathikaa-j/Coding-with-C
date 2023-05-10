f1d0f91ba5acab592d7c2635f1556f12eb40e146
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2023-05-10 18:18:58 +0530

    Create forloop.c
    Pattern using nested loop

---
+++ forloop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,b;
+    for (a=1;a<=5;a++)
+    {
+        for(b=1;b<=10;b++)
+        {
+            printf("*");
+
+        }
+            printf("\n");
+    }
+    return 0;
+}
