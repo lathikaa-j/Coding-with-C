6e796c662a5beb9e4615f878867e823aff819c8f
Lathikaa J <130064380+lathikaa-j@users.noreply.github.com>
2023-05-10 18:45:05 +0530

    Create uppercase_to_lowercase.c
    Converting the Uppercase characters to lowercase

---
+++ uppercase_to_lowercase.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+
+    char k;
+    printf("Enter any character in uppercase:");
+    scanf("%c",&k);
+    printf("The character in lowercase is %c ",k+32);
+
+    return 0;
+}
